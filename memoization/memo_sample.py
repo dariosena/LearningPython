694855e1b33a115aaae3f1aba276e6106e455774
Emanuel D R Sena <dario@gtel.ufc.br>
2017-09-28 23:51:19 -0300

    Learning memoization!

---
+++ memoization/memo_sample.py
@@ -0,0 +1,25 @@
+import time
+
+ef_cache = {}
+
+def expensive_func(num):
+    if num in ef_cache:
+        return ef_cache[num]
+
+    print('Computing {}...'.format(num))
+    result = num * num
+    time.sleep(1)
+    ef_cache[num] = result
+    return result
+
+result = expensive_func(4)
+print(result)
+
+result = expensive_func(10)
+print(result)
+
+result = expensive_func(4)
+print(result)
+
+result = expensive_func(10)
+print(result)
