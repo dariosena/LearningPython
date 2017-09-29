d18d4c9872bb8ca5a9afe84051aaca544e48a348
Emanuel D R Sena <dario@gtel.ufc.br>
2017-09-28 23:46:33 -0300

    Learning closure sintax in python.

---
+++ closure/basic_closure.py
@@ -0,0 +1,17 @@
+# Closures
+
+def outer_func(msg):
+    message = msg
+
+    def inner_func():
+        print(message)
+
+    return inner_func
+
+hi_func = outer_func('Hi')
+hello_func = outer_func('Hello')
+
+print(hi_func.__name__)
+
+hi_func()
+hello_func()
