176e77e9f10b7af0faeefd737ad9f02a3d349a92
Emanuel D R Sena <dariosena@gmail.com>
2017-10-14 14:13:15 -0300

    Learning DRY.

--- dry/sample.py
+++ dry/sample.py
@@ -3,29 +3,35 @@ def nav_menu():
     print('<a href="#">About</a>')
     print('<a href="#">Contact</a>')
 
+
 def header():
     print('<div class="header">')
     nav_menu()
     print('</div>')
+
 
 def footer():
     print('<div class="footer">')
     nav_menu()
     print('</div>')
 
+
 def homePage():
     header()
     print('<p>Welcome to our Home Page</p>')
     footer()
+
 
 def aboutPage():
     header()
     print('<p>Welcome to our About Page</p>')
     footer()
 
+
 def contactPage():
     header()
     print('<p>Welcome to our Contact Page</p>')
     footer()
+
 
 homePage()
--- dry/test_calc.py
+++ dry/test_calc.py
@@ -1,8 +1,10 @@
 import unittest
 import calc
 
+
 class CalcTestCase(unittest.TestCase):
     """Test calc.py"""
+
     def setUp(self):
         self.num1 = 10
         self.num2 = 5
@@ -15,15 +17,16 @@ class CalcTestCase(unittest.TestCase):
 
     def test_subtract(self):
         self.assertTrue(calc.subtract(self.num1, self.num2),
-                                      self.num1 - self.num2)
+                        self.num1 - self.num2)
 
     def test_multiply(self):
         self.assertTrue(calc.multiply(self.num1, self.num2),
-                                      self.num1 * self.num2)
+                        self.num1 * self.num2)
 
     def test_divide(self):
         self.assertTrue(calc.divide(self.num1, self.num2),
-                                    self.num1 / self.num2)
+                        self.num1 / self.num2)
+
 
 if __name__ == '__main__':
     unittest.main()
