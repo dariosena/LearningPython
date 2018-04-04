c7fd75076595658959bb57699ea3791f9423fba0
Emanuel D. R. Sena <dario@gtel.ufc.br>
2018-04-04 00:27:31 -0300

    Auto stash before merge of "master" and "origin/master"
    Learning basics.

---
+++ lpthw/ex9.py
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, ...
+
+days = "Mon Tue Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+Several
+Lines
+For
+One
+String
+!!!
+""")
