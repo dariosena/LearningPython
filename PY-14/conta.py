c836fbe5e8ac4b18f132afa608428d88efc939d1
Emanuel Sena <dariosena@gmail.com>
2019-09-23 12:50:23 -0300

    Learning OO.

--- PY-14/conta.py
+++ PY-14/conta.py
@@ -23,22 +23,22 @@ class Cliente:
 class Conta:
     def __init__(self, numero, cliente, saldo, limite=1000.0):
         print('inicializando uma conta')
-        self.numero = numero
-        self.titular = cliente
-        self.saldo = saldo
-        self.limite = limite
-        self.historico = Historico()
+        self._numero = numero
+        self._titular = cliente
+        self._saldo = saldo
+        self._limite = limite
+        self._historico = Historico()
         
     def deposita(self, valor):
-        self.saldo += valor
-        self.historico.transacoes.append('depósito de {}'.format(valor))
+        self._saldo += valor
+        self._historico.transacoes.append('depósito de {}'.format(valor))
         
     def saca(self, valor):
-        if (self.saldo < valor):
+        if (self._saldo < valor):
             return False
         else:
-            self.saldo -= valor
-            self.historico.transacoes.append('saque de {}'.format(valor))
+            self._saldo -= valor
+            self._historico.transacoes.append('saque de {}'.format(valor))
             return True
         
     def transfere_para(self, destino, valor):
@@ -46,11 +46,11 @@ class Conta:
         
         if (retirou):
             destino.deposita(valor)
-            self.historico.transacoes.append('transferencia de {} para conta'.format(valor, destino.numero))
+            self._historico.transacoes.append('transferencia de {} para conta'.format(valor, destino.numero))
             return True
         else:
             return False
         
     def extrato(self):
-        print('numero: {} \nsaldo: {}'.format(self.numero, self.saldo))
-        self.historico.transacoes.append('tirou extrato - saldo de {}'.format(self.saldo))
+        print('numero: {} \nsaldo: {}'.format(self._numero, self._saldo))
+        self._historico.transacoes.append('tirou extrato - saldo de {}'.format(self._saldo))
